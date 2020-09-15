1ad8e122c29a413d03ea813a1c2ba65b9a44f63c
Nick Floyd <nicholas.floyd@gmail.com>
2020-09-15 00:36:16 -0500

    adds tests

---
+++ SearchProjectUnitTests/Models/SearchResultsTests.cs
@@ -0,0 +1,22 @@
+using NUnit.Framework;
+using SearchProject.Models;
+
+namespace SearchProjectUnitTests.Models
+{
+    [TestFixture]
+    public class SearchResultsTests
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            // Arrange
+            var searchResults = new SearchResults();
+
+            // Act
+
+
+            // Assert
+            Assert.Fail();
+        }
+    }
+}
